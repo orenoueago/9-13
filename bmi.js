e7664ba3543a288fc90729d6a3ad89e112775dbf
orenoueago <KkcyclonekK@gmail.com>
2024-09-20 15:40:04 +0800

    messagee

--- bmi.js
+++ bmi.js
@@ -1,9 +1,11 @@
 
 function calcbmi(){
-var weight=document.getElementById("weight")
-var height=document.getElementById("height")
+var weight = parseInt(document.getElementById("weight").value);
+var height = parseInt(document.getElementById("height").value);
 var bmi=weight/((height/100)**2);
+document.getElementById("bmi").innerHTML = bmi;
 
-console.log ('hello,your bmi is'+bmi);
+console.log ('hello,your bmi is:'+ bmi);
 
 }
+IPO
