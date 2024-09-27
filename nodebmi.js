176ca2dca2c593384f7d454233339944d195830e
orenoueago <KkcyclonekK@gmail.com>
2024-09-27 15:27:32 +0800

    mwssage

--- nodebmi.js
+++ nodebmi.js
@@ -1,7 +1,7 @@
 var readline = require('readline-sync');
 
 var weight = readline.question('please enter your weight(KG)?');
-var height = 160;
+var height = readline.question('please enter your height(cm)?');
 var bmi=weight/((height/100)**2);
 //document.getElementById("bmi").innerHTML = bmi;
 
