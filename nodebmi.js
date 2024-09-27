5681bb12d0d22c7df37e6d69fc9e9e87e30c19a8
orenoueago <KkcyclonekK@gmail.com>
2024-09-27 15:35:31 +0800

    message

--- nodebmi.js
+++ nodebmi.js
@@ -2,8 +2,18 @@ var readline = require('readline-sync');
 
 var weight = readline.question('please enter your weight(KG)?');
 var height = readline.question('please enter your height(cm)?');
-var bmi=weight/((height/100)**2);
+var bmi=Math.floor(weight/((height/100)**2)*10)/10;
 //document.getElementById("bmi").innerHTML = bmi;
 
 console.log ('hello,your bmi is:'+ bmi);
+
+if(bmi<=18.5){
+    system.out.println("體重過瘦");
+}else if(bmi>=18.5 && bmi<24){
+    system.out.println("體重適中");
+}else {
+    system.out.println("體重過重");
+}
+
+
  
