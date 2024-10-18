54f6aa2dc710d802ffbcf2b48cfd5da825ddbc36
orenoueago <KkcyclonekK@gmail.com>
2024-10-18 14:35:18 +0800

    message

--- 2dAry4html.js
+++ 2dAry4html.js
@@ -13,11 +13,11 @@ var col=ary2d[0].length;
 var canvas = document.getElementById("map").getContext("2d");
 var widthsize=canvas.width/row
 var lengthsize=canvas.length/row
-for(var _raw=0;_raw<row;_raw++)
+for(var _row=0;_row<row;_row++)
 {
     for(var _col=0;_col<col;_col++)
     {
-        dynaAry2d[_raw].push(_col+_raw*col);
+        ary2d[_row].push(_col+_row*col);
         if(ary2d[_row][_col]==1){
             canvas.fillStyle="#0000ff"
         }else{
